e5d7c578713c01a05fe563a9ac2afc8eb31af1fe
Leonard Tkatchenko <leonard.tkatchenko@gmail.com>
2020-11-07 08:56:08 +1100

    work in progress of image grid

--- WDP20207175A3/wwwroot/js/site.js
+++ WDP20207175A3/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function makeImageBlur(obj) {
+    obj.style.opacity = 0.5;
+}
+
+function makeImageUnblur(obj) {
+    obj.style.opacity = 1;
+    document.getElementById("bgimage").style.background = "lightgrey";
+}
+
+function displayBigImage(obj) {
+    document.getElementById("bgimage").style.backgroundImage = obj.style.backgroundImage;
+    document.getElementById("bgimage").style.backgroundSize = "100%";
+    document.getElementById("bgimage").style.backgroundRepeat = no - repeat;
+    document.getElementById("bgimage").style.opacity = 1; //unnecessary code?
+
+}
