50b77d6955664a6c4540d84cf978f83fc0b62e92
Leonard Tkatchenko <leonard.tkatchenko@gmail.com>
2020-11-07 05:26:09 +1100

    Changed privacy page to restaurants. Updated nav links and reworded footer

--- WDP20207175A3/Controllers/HomeController.cs
+++ WDP20207175A3/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace WDP20207175A3.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Restaurants()
         {
             return View();
         }
